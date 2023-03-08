b7b16713bd484c95bfaf796d31a28abcdc5e7c51
Shreyak Chakraborty <shreyak.rekshda@gmail.com>
2023-03-08 13:10:08 +0530

    added logs for player actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
+import { useState } from 'react'
 import './App.css'
 import Player from './components/player/Player'
+import { gameEvent } from './types';
 
 function App() {
-
+  const [gameEvents,setGameEvents] = useState(["Game started"]); 
+  const onPlayerAction = (event: string,data: any) => {
+    setGameEvents([...gameEvents,event]);
+  }
   return (
     <div className="App">
         <h1 style={{
           marginBottom: 100
         }}>RPG Battle</h1>
-        <Player />
-        <Player playerName='You' />
+
+        <div className='alert'><b>Game logs</b> <br />
+        <div id='logsbox' className='eventlogs'>
+          {
+            gameEvents.map((logItem) => (
+              <div key={logItem}>{logItem}</div>
+            ))
+          }
+        </div>
+        </div>
+
+        <Player onPlayerAction={onPlayerAction} />
+        <Player playerName='You' onPlayerAction={onPlayerAction} />
     </div>
     
   )
--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -1,15 +1,34 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import heartIcon from "../../assets/icons/heart.svg";
 import swordIcon from "../../assets/icons/sword.svg";
 import shieldIcon from "../../assets/icons/shield.svg";
 import magicIcon from "../../assets/icons/magic.svg";
 
-export default function Player({playerName="CPU",playerColor="blue"}) {
+import {utils} from "../../utils";
+
+export default function Player({playerName="CPU",playerColor="blue",onPlayerAction}) {
     const [playerHP,setPlayerHP] = useState(50); //health points
     const [playerAttack,setPlayerAttack] = useState(0);
     const [playerDefence,setPlayerDefence] = useState(0);
     const [playerMP,setPlayerMP] = useState(0); // mana points
+
+    const [playerInventory,setPlayerInventory] = useState([]);
+    const [showInventory,setShowInventory] = useState(false);
+
+    const toggleInventory = () => {
+        setShowInventory(!showInventory);
+    }
+
+    useEffect(() => {
+        const totalPoints = 10;
+        const attackPower = utils.getRandomInt(2,8);
+        setPlayerAttack(attackPower);
+        setPlayerDefence(totalPoints - attackPower);
+
+    },[playerAttack,playerDefence])
+
     return (
+        
         <div className="card mb-5">
             <span style={{
                 color: playerColor
@@ -31,9 +50,36 @@ export default function Player({playerName="CPU",playerColor="blue"}) {
             </div>
 
             <div className="playeroptions">
-                <button className="btn btn-primary">Attack</button>
-                <button className="btn btn-warning">Defend</button>
+                <button className="btn btn-primary" onClick={() => {
+                    onPlayerAction(playerName + " attacked")
+                }}>Attack</button>
+                <button className="btn btn-warning" onClick={() => {
+                    onPlayerAction(playerName + " defended")
+                }}>Defend</button>
             </div>
+            <div className="playeroptions">
+                <button className="btn btn-success" onClick={toggleInventory}> {showInventory ? "Close" : "Open"} Inventory</button>
+            </div>
+
+            {
+                showInventory && (
+                    <div className="inventory card">
+                        {
+                            playerInventory.length == 0 ? (
+                                <b>Inventory is empty</b>
+                            )
+                            : (
+                                playerInventory.map((item) => (
+                                    <div key={item} className="inventory-item">
+                                        Item
+                                    </div>
+                                ))
+                            )
+                        }
+                    </div>
+                )
+            }
         </div>
+        
     )
 }
